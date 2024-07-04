a4df9908eefff13ab57e19ee45daf34b8e5dfd9a
vphson <vophuochoangson@gmail.com>
2024-07-04 15:55:59 +0700

    Add Distance ViewController

--- ObjectDetection-CoreML/TabBarController.swift
+++ ObjectDetection-CoreML/TabBarController.swift
@@ -13,20 +13,15 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Create instances of view controllers
         let firstVC = DetectNailViewController()
-        let secondVC = SecondViewController()
+        let secondVC = DistanceViewController()
 
+        firstVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        secondVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
 
-        // Configure tab bar items for each view controller
-        firstVC.tabBarItem = UITabBarItem(title: "First", image: UIImage(named: "firstIcon"), tag: 0)
-        secondVC.tabBarItem = UITabBarItem(title: "Second", image: UIImage(named: "secondIcon"), tag: 1)
-
-        // Wrap view controllers in navigation controllers for additional functionality
         let nav1 = UINavigationController(rootViewController: firstVC)
         let nav2 = UINavigationController(rootViewController: secondVC)
 
-        // Assign view controllers to the tab bar
         viewControllers = [nav1, nav2]
     }
 }
