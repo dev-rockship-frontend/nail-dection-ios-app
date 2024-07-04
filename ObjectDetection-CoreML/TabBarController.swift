46a90969ddcb24362d7e5a0bc8291014f2a0ac4f
vphson <vophuochoangson@gmail.com>
2024-07-04 16:27:53 +0700

    Add tarbar icon

--- ObjectDetection-CoreML/TabBarController.swift
+++ ObjectDetection-CoreML/TabBarController.swift
@@ -11,17 +11,17 @@ import UIKit
 class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
 
-        let firstVC = DetectNailViewController()
-        let secondVC = DistanceViewController()
+            let firstVC = DetectNailViewController()
+            let secondVC = DistanceViewController()
 
-        firstVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        secondVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        firstVC.tabBarItem = UITabBarItem(title: "Detect Nails", image: UIImage(named: "detectIcon"), tag: 0)
+        secondVC.tabBarItem = UITabBarItem(title: "Distance", image: UIImage(named: "distanceIcon"), tag: 1)
 
-        let nav1 = UINavigationController(rootViewController: firstVC)
-        let nav2 = UINavigationController(rootViewController: secondVC)
+            let nav1 = UINavigationController(rootViewController: firstVC)
+            let nav2 = UINavigationController(rootViewController: secondVC)
 
-        viewControllers = [nav1, nav2]
-    }
+            viewControllers = [nav1, nav2]
+        }
 }
