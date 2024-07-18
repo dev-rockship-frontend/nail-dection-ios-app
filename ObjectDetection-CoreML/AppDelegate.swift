d7065eb7b4c1ec5397a68e1b8899681aeb875c72
vphson <vophuochoangson@gmail.com>
2024-07-18 09:54:36 +0700

    Add IQKeyboardManager

--- ObjectDetection-CoreML/AppDelegate.swift
+++ ObjectDetection-CoreML/AppDelegate.swift
@@ -6,39 +6,21 @@
 //  Copyright © 2024 tucan9389. All rights reserved.
 //
 
-//
-//  AppDelegate.swift
-//  ObjectDetection-CoreML
-//
-//  Created by Huy Dang on 22/6/24.
-//  Copyright © 2024 tucan9389. All rights reserved.
-//
-
 
 import UIKit
-
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    //    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    //        window = UIWindow(frame: UIScreen.main.bounds)
-    //
-    //                // Set the TabBarController as the root view controller
-    //                let tabBarController = TabBarController()
-    //                window?.rootViewController = tabBarController
-    //                window?.makeKeyAndVisible()
-    //
-    //        return true
-    //    }
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let mainViewController = DetectNailViewController()
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
+        IQKeyboardManager.shared.enable = true
         
         return true
     }
--- Podfile
+++ Podfile
@@ -5,5 +5,5 @@ target 'ObjectDetection-CoreML' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
   pod 'SnapKit'
-  pod 'Charts'
+  pod 'IQKeyboardManagerSwift', '6.3.0'
 end
